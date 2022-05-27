851d08b0d8b71bce388e20e6abb7a910c4959931
John Lundgren <john.lundgren.lexicon@gmail.com>
2022-05-27 15:03:42 +0200

    Added Navbar tests.

---
+++ src/components/Navbar/__test__/Navbar.test.js
@@ -0,0 +1,26 @@
+import { render, screen } from '@testing-library/react';
+import Navbar from '../Navbar';
+
+it('should render same text passed into title prop', () => {
+  render(<Navbar title="Use your illusion" />);
+  const titleElement = screen.getByText(/use your illusion/i);
+  expect(titleElement).toBeInTheDocument();
+});
+
+it('should not render user name in navbar when user is null', () => {
+  render(<Navbar title="The title" user={null} />);
+  const userElement = screen.queryByText(/user name/i);
+  expect(userElement).not.toBeInTheDocument();
+});
+
+it('should render user name passed into user prop', () => {
+  render(<Navbar title="The title" user="user name" />);
+  const userElement = screen.getByText(/user name/i);
+  expect(userElement).toBeInTheDocument();
+});
+
+it('should render log in button in navbar when user is null', () => {
+  render(<Navbar title="The title" user={null} />);
+  const loginButtonElement = screen.getByRole('button', { name: 'Log in' });
+  expect(loginButtonElement).toBeInTheDocument();
+});
