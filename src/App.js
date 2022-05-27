d0d2851c1cb199b0d52a7dbb3b43e05f8cd66333
John Lundgren <john.lundgren.lexicon@gmail.com>
2022-05-27 10:44:34 +0200

    Preparation for Navbar tests.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import Navbar from './components/Navbar';
+import Navbar from './components/Navbar/Navbar';
 import BooksCollection from './components/BooksCollection';
 import bookData from './data/books';
 import { useState } from 'react';
