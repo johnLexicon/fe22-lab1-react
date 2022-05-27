9ca7eaefb42cc89df27f75b7bba0889148075ff9
John Lundgren <john.lundgren.lexicon@gmail.com>
2022-05-27 16:45:08 +0200

    Using jest mock function for required prop in navbar.

--- src/components/Navbar/__test__/Navbar.test.js
+++ src/components/Navbar/__test__/Navbar.test.js
@@ -2,26 +2,40 @@ import { render, screen } from '@testing-library/react';
 import Navbar from '../Navbar';
 
 describe('Navbar tests', () => {
+  const mockedHandleLogin = jest.fn;
+
   it('should render same text passed into title prop', () => {
-    render(<Navbar title="Use your illusion" />);
+    render(
+      <Navbar title="Use your illusion" handleLogin={mockedHandleLogin} />
+    );
     const titleElement = screen.getByText(/use your illusion/i);
     expect(titleElement).toBeInTheDocument();
   });
 
   it('should not render user name in navbar when user is null', () => {
-    render(<Navbar title="The title" user={null} />);
+    render(
+      <Navbar title="The title" user={null} handleLogin={mockedHandleLogin} />
+    );
     const userElement = screen.queryByText(/user name/i);
     expect(userElement).not.toBeInTheDocument();
   });
 
   it('should render user name passed into user prop', () => {
-    render(<Navbar title="The title" user="user name" />);
+    render(
+      <Navbar
+        title="The title"
+        user="user name"
+        handleLogin={mockedHandleLogin}
+      />
+    );
     const userElement = screen.getByText(/user name/i);
     expect(userElement).toBeInTheDocument();
   });
 
   it('should render log in button in navbar when user is null', () => {
-    render(<Navbar title="The title" user={null} />);
+    render(
+      <Navbar title="The title" user={null} handleLogin={mockedHandleLogin} />
+    );
     const loginButtonElement = screen.getByRole('button', { name: 'Log in' });
     expect(loginButtonElement).toBeInTheDocument();
   });
